7b3b17811b371d1c12862b3730a3c3393c46f5d4
asarium <asarium@gmail.com>
2017-06-24 12:02:43 +0200

    Move prebuilt binaries to GitHub releases
    This changes the workflow so that Travis CI pushes a new tag for every
    commit to the repository and then for that commit it will package the
    binaries and upload them to that commit for GitHub.
    
    The GitHub API token was generated by the SirKnightly account which
    should be able to create the releases.

--- ci/script.sh
+++ ci/script.sh
@@ -2,10 +2,21 @@
 
 set -ex
 
-mkdir -p /tmp/packages
+if [ -n "$TRAVIS_TAG" ]; then 
+	mkdir -p /tmp/packages
 
-(cd linux && tar -cvzf /tmp/packages/bin-$TRAVIS_COMMIT-linux.tar.gz *)
-(cd mac && tar -cvzf /tmp/packages/bin-$TRAVIS_COMMIT-mac.tar.gz *)
+	(cd linux && tar -cvzf /tmp/packages/bin-linux.tar.gz *)
+	(cd mac && tar -cvzf /tmp/packages/bin-mac.tar.gz *)
 
-(cd win32 && zip -r /tmp/packages/bin-$TRAVIS_COMMIT-win32.zip *)
-(cd win64 && zip -r /tmp/packages/bin-$TRAVIS_COMMIT-win64.zip *)
+	(cd win32 && zip -r /tmp/packages/bin-win32.zip *)
+	(cd win64 && zip -r /tmp/packages/bin-win64.zip *)
+else
+	# Make sure pull requests don't trigger this. Should be disabled from the
+	# dashboard but lets make sure that this doesn't happen
+	if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then exit 1; fi
+
+	# Only tags create the packages, normal pushes just create the tags
+	git remote add deploy "git@github.com:scp-fs2open/scp-prebuilt.git"
+	git tag -a "bin-$TRAVIS_COMMIT" -m "Automated prebuilt binary tag"
+	git push deploy --tags
+fi
